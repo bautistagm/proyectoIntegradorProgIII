2a99a1b0bbc6cd9e8dfcf5925658388de61ebe15
antoniocaoo <acaolavignolle@udesa.edu.ar>
2024-09-16 15:47:07 -0300

    un poco de css de footer y empiezo con el header

--- boiler-plate/src/components/Footer/Footer.js
+++ boiler-plate/src/components/Footer/Footer.js
@@ -1,14 +1,14 @@
 import React from "react";
-
+import "./Footer.css"
 
 function Footer(){
 
     return ( 
 
-        <footer>
+        <footer className="foot">
         <p>Copyright © Dashboard 2024</p>
     
-        <h5>Antonio Cao Lavignolle, Fermin Garcia Belmonte, Bautista Garcia Mansilla</h5>
+        <h4>Antonio Cao Lavignolle, Fermin Garcia Belmonte, Bautista Garcia Mansilla</h4>
 
         </footer>
     )
--- boiler-plate/src/components/Header/Header.js
+++ boiler-plate/src/components/Header/Header.js
@@ -1,5 +1,30 @@
 import { Component } from "react";
 
 
+const Header = () => {
+
+    return (
+
+    <nav>
+        <ul className="main-nav">
+           
+            <li>elemento menu</li>
+            <li>elemento menu</li>
+            <li>elemento menu</li>
+            <li>elemento menu</li>
+        </ul>
+        <ul className="user">
+            <li>Nombre usuario <img src="" alt="" /></li>
+        </ul>
+    </nav>
+
+
+    )
+
+    
+
+}
+
+
 
 export default Header;
