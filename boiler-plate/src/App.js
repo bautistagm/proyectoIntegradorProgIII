dbe7e7a8e3c55ea7b2cf8eb135056df306c80b2a
antoniocaoo <acaolavignolle@udesa.edu.ar>
2024-09-16 11:50:02 -0300

    implemento footer en app.js, con lo que pide la consigna

--- boiler-plate/src/App.js
+++ boiler-plate/src/App.js
@@ -1,6 +1,12 @@
+import React from "react";
+
+
+import Footer from "./components/Footer/Footer";
+
 function App() {
   return (
-    <></>
+   
+    <Footer/>
   );
 }
 
--- boiler-plate/src/components/Footer/Footer.js
+++ boiler-plate/src/components/Footer/Footer.js
@@ -1,4 +1,21 @@
-import { Component } from "react";
+import React from "react";
+
+
+function Footer(){
+
+    return ( 
+
+        <footer>
+        <p>Copyright © Dashboard 2024</p>
+    
+        <h5>Antonio Cao Lavignolle, Fermin Garcia Belmonte, Bautista Garcia Mansilla</h5>
+
+        </footer>
+    )
+
+
+
+}
 
 
 
