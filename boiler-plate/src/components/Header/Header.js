5cfb2cf465d3d9d8a1504e463b35137eec9af3a9
antoniocaoo <acaolavignolle@udesa.edu.ar>
2024-09-20 12:43:51 -0300

    buscador en el header, avanzado, faltan detalles

--- boiler-plate/src/components/Header/Header.js
+++ boiler-plate/src/components/Header/Header.js
@@ -1,31 +1,66 @@
 import { Component } from "react";
 import "./Header.css"
-
-const Header = () => {
-
-    return (
-
-    <nav>
-
-        <ul>
-            <img src="/img/cheflix.jpg" alt="Logo" className="logo" />
-        </ul>
-        <ul className="main-nav">
-           
-            <li>Detalle</li>
-            <li>Favoritos</li>
-            <li>Ver todas</li>
-            
-        </ul>
-        <ul className="user">
-            <li>Nombre usuario <img src="" alt="" /></li>
-        </ul>
-    </nav>
+import SearchForm from "../SearchForm/SearchForm";
 
 
-    )
+class Header extends Component {
 
-    
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            query: ""
+        }
+    }
+
+    handleFormChange(e) {
+        this.setState({
+            query: e.target.value
+        })
+    }
+
+    handleCancelSubmit(e) {
+        e.preventDefault()
+    }
+
+    handleFormSubmit() {
+        this.props.history.push("/search", { query: this.state.query })
+    }
+
+    render() {
+        return (
+            <>
+
+                <nav>
+
+                    <ul>
+                        <img src="/img/cheflix.jpg" alt="Logo" className="logo" />
+                    </ul>
+                    <ul className="main-nav">
+
+                        <li>Detalle</li>
+                        <li>Favoritos</li>
+                        <li>Ver todas</li>
+
+                    </ul>
+
+                    <div className="fondo">
+                        <SearchForm history={this.props.history} />
+                        <form onSubmit={(e) => this.handleCancelSubmit(e)}>
+                            <input onChange={(e) => this.handleFormChange(e)} name="query" value={this.state.query} />
+                            <button onClick={() => this.handleFormSubmit}>Search</button>
+                        </form>
+                    </div>
+                    
+                    <ul className="user">
+                        <li>Nombre usuario <img src="" alt="" /></li>
+                    </ul>
+                </nav>
+
+            </>
+        )
+
+    }
 
 }
 
--- boiler-plate/src/pages/Home.js
+++ boiler-plate/src/pages/Home.js
@@ -1,42 +1,16 @@
 import PeliculasGrid from "../components/PeliculasGrid/PeliculasGrid";
 import { Component } from "react"
 import SearchForm from "../components/SearchForm/SearchForm";
-
+import "../pages/styles.css"
 
 class Home extends Component {
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            query:""
-        }
-    }
-
-    handleFormChange(e){
-        this.setState({
-            query: e.target.value
-        })
-    }
-
-    handleCancelSubmit(e){
-        e.preventDefault()
-    }
-
-    handleFormSubmit(){
-        this.props.history.push("/search", { query: this.state.query })
-    }
+    
 
     render() {
       return (
         <>
-         <div className="fondo">
-                <SearchForm history={this.props.history}/>
-                <form onSubmit={(e)=>this.handleCancelSubmit(e)}>
-                    <input onChange={(e)=>this.handleFormChange(e)} name = "query" value={this.state.query}/>
-                    <button onClick={()=>this.handleFormSubmit}>Search</button>
-                </form>
-            </div>
+         
           <h1>Bienvenidos a Cheflix!</h1>
           <main>
             <h2>Peliculas más Populares</h2>
